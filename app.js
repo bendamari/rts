4e2a9a12bf10ec77b76e761875f0de27d8fffb04
readbee <yoni@read-bee.comgit config --global user.name Yoni>
2018-06-28 12:58:00 +0300

    ubuntu

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-
+//testd
 //database connection requirements
 const pool = require('./db');
 
